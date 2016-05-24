c9692014f93c9555e09f8a032e0ca258250fca33
motofondue <motofondue@no-log.org>
2016-05-24 15:03:24 +0200

    Test method with unknow amount number and newline separator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,20 +1,26 @@
 
 public class StringCalculator {
 
+	private static final String EMPTY_STRING = "";
+	private static final String NEW_LINE_STRING = System.getProperty("line.separator");
+	private static final String STRING_SEPARATOR = ",";
+
 	// Sum function from String
 	public int add(String numbers) {
 		// Check empty string
-		if (numbers.equals("")) {
+		if (numbers.equals(EMPTY_STRING)) {
 			return 0;
 		}
 		
 		// Parse string
-		String[] nums = numbers.split(",");
-		if (nums.length == 1) {
-			return Integer.valueOf(nums[0]);
+		String[] nums = numbers.split("("+STRING_SEPARATOR+"|"+NEW_LINE_STRING+")");
+		int total = 0;
+		// Loop on length numbers given
+		for (String num : nums) {
+			total += Integer.valueOf(num);
 		}
 		
-		// Default return
-		return -1;
+		// Return sum
+		return total;
 	}
 }
--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -4,21 +4,31 @@ import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
 
-	private StringCalculator sc;
+	private StringCalculator stringCalculator;
     private static final String EMPTY_STRING = "";
-    private static final String SINGLE_NUMBER_2 = "2";
+	private static final String NEW_LINE_STRING = System.getProperty("line.separator");
+    
+	private static final String SINGLE_NUMBER_2 = "2";
+    private static final String TWO_NUMBER_GIVEN = "1,2";
 
+    private static final String MULTIPLE_NUMBER_GIVEN_1 = "1,2,3,4,5,6";
+    private static final String MULTIPLE_NUMBER_GIVEN_2 = "100,-100";
+    private static final String MULTIPLE_NUMBER_GIVEN_3 = 
+    		"9"+NEW_LINE_STRING+
+    		"11"+NEW_LINE_STRING+
+    		"1"+NEW_LINE_STRING+
+    		"2";
 
     @Before
 	public void setup(){
 		// Given
-		sc = new StringCalculator();
+		stringCalculator = new StringCalculator();
 	}
 
 	@Test
 	public void shouldBeZeroWhenStringIsEmpty() {
 		// When
-		int sum = sc.add(EMPTY_STRING);
+		int sum = stringCalculator.add(EMPTY_STRING);
 		// Then
 		assertEquals(0, sum);
 	}
@@ -26,8 +36,28 @@ public class StringCalculatorTest {
 	@Test
 	public void shouldBeTwoWhenTwoGiven() {
 		// When
-		int sum = sc.add(SINGLE_NUMBER_2);
+		int sum = stringCalculator.add(SINGLE_NUMBER_2);
 		// Then
 		assertEquals(2, sum);
+	}
+
+	@Test
+	public void shouldBeThreeWhenOneAndTwoGiven() {
+		// When
+		int sum = stringCalculator.add(TWO_NUMBER_GIVEN);
+		// Then
+		assertEquals(3, sum);
+	}
+
+	@Test
+	public void shouldBeSumWhenMultipleNumberGiven() {
+		// When
+		int sum1 = stringCalculator.add(MULTIPLE_NUMBER_GIVEN_1);
+		int sum2 = stringCalculator.add(MULTIPLE_NUMBER_GIVEN_2);
+		int sum3 = stringCalculator.add(MULTIPLE_NUMBER_GIVEN_3);
+		// Then
+		assertEquals(21, sum1);
+		assertEquals(0, sum2);
+		assertEquals(23, sum3);
 	}
 }
