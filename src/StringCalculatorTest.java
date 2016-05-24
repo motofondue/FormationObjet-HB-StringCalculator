82c977e1b1bff22bdd48cdf44c9ed1fd414d1490
yomat <matthias.mailliard@gmail.com>
2016-05-24 14:41:36 +0200

    UPDATE StringCalculatorTest.java
    some enhancements

--- src/StringCalculatorTest.java
+++ src/StringCalculatorTest.java
@@ -1,25 +1,32 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class StringCalculatorTest {
-	
+
+	private StringCalculator sc;
+    private static final String EMPTY_STRING = "";
+    private static final String SINGLE_NUMBER_2 = "2";
+
+
+    @Before
+	public void setup(){
+		// Given
+		sc = new StringCalculator();
+	}
+
 	@Test
 	public void shouldBeZeroWhenStringIsEmpty() {
-		// Given
-		StringCalculator sc = new StringCalculator();
 		// When
-		int sum = sc.add("");
+		int sum = sc.add(EMPTY_STRING);
 		// Then
 		assertEquals(0, sum);
 	}
-	
+
 	@Test
 	public void shouldBeTwoWhenTwoGiven() {
-		// Given
-		StringCalculator sc = new StringCalculator();
 		// When
-		int sum = sc.add("2");
+		int sum = sc.add(SINGLE_NUMBER_2);
 		// Then
 		assertEquals(2, sum);
 	}
